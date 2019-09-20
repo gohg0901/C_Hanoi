e48433f48d54daee1105cf4000b24ff6bbbdc0c7
gohg0901 <gohg0901@gmail.com>
2019-09-21 00:29:20 +0900

    수정중

--- Hanoi_pj/소스.c
+++ Hanoi_pj/소스.c
@@ -1,24 +1,67 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void hanoi(int n, char from, char tmp, char to) {
+
+typedef struct _stack {
+	int s[11];
+	int top;
+
+}Stack;
+
+void push(Stack* a, int i) {
+
+	a->top = a->top + 1;
+	a->s[a->top] = i;
+}
+
+int pop(Stack* a) {
+
+	a->top = a->top - 1;
+	return a->s[a->top + 1];
+}
+
+void hanoi(int n, Stack *from, Stack *tmp, Stack *to) {
 	
 	if (n == 1) {
-		printf("원반 1를 %c -> %c \n",from,to);
+		push(to, 1);
+		pop(from);
+
+		
 	}
 	else {
 		hanoi(n - 1, from, to, tmp);
-		printf("원반 %d를 %c -> %c \n",n,from,to);
+		printf("원반 %d를 %d -> %d \n",n,from,to);
+		printf("-----A-----     -----B-----     -----C-----\n");
 		hanoi(n - 1, tmp, from, to);
 	}
 }
 
 main() {
 	int n;
+	int x;
 
-	printf("input : ");
+	Stack from;
+	Stack tmp;
+	Stack to;
+
+	from.top = -1;
+	tmp.top = -1;
+	to.top = -1;
+
+
+	printf("input (1 <= n <= 10 ) : ");
 	scanf("%d",&n);
-	
 
-	hanoi(n, 'A', 'B', 'C');
+	//push(&A, 8);
+	//x = pop(&A);
+	//printf("%d \n", x);
+
+
+	hanoi(n, &from, &tmp, &to);
+	printf("-----A-----     -----B-----     -----C-----\n");
+
+
+
+
+
 }
