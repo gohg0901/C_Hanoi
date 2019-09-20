53d4e04f2943eb742445b776c059e7397fc0a8cf
gohg0901@office.bufs.ac.kr <gohg0901@office.bufs.ac.kr>
2019-09-20 23:46:52 +0900

    print Hanoi text line

--- Hanoi_pj/소스.c
+++ Hanoi_pj/소스.c
@@ -1,13 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
+void hanoi(int n, char from, char tmp, char to) {
+	
+	if (n == 1) {
+		printf("원반 1를 %c -> %c \n",from,to);
+	}
+	else {
+		hanoi(n - 1, from, to, tmp);
+		printf("원반 %d를 %c -> %c \n",n,from,to);
+		hanoi(n - 1, tmp, from, to);
+	}
+}
 
 main() {
 	int n;
 
+	printf("input : ");
+	scanf("%d",&n);
 	
-	
-
-
-
+	hanoi(n, 'A', 'B', 'C');
 }
